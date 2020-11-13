f1485ad26e5698e4b1e469986f8eb5507de512c3
Jana Zrayka <janjoune.97@hotmail.com>
2020-11-13 18:07:06 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,15 +1,6 @@
 // Code your solutions in this file
 
-function writeCards(names, event){
- for(let i=0;i<names.length;i++){
-   
- }
-     
-   return "Thank you, " + names[i] + " for the wonderful " + event+ " gift !";
-  
-}
-writeCards( [ 'Lisa', 'Kaitlin', 'Jan' ], "surprise");
-
+      
 function countDown(number){
   while(number<=10 && number>=0){
     console.log(number--);
