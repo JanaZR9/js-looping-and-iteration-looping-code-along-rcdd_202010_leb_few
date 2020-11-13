7e061a4620ecfad5694a140dfb52c3a9bcb63f2c
Jana Zrayka <janjoune.97@hotmail.com>
2020-11-13 18:02:39 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ function writeCards(names, event){
  for(let i=0;i<names.length;i++)
   {
      
-    console.log(`Thank you, ${names[i]} for the wonderful ${event} gift !`);
+   return "Thank you," +names
   }
   
 }
