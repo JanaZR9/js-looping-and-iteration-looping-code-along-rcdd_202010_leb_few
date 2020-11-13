c9c95d66a0704cff4a1c86bd503073b027f9b383
Jana Zrayka <janjoune.97@hotmail.com>
2020-11-13 18:07:42 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,7 +2,8 @@
 
 function writeCards(names, event){
  for(let i=0;i<names.length;i++){
-   
+    console.log(`Thank you, ${names[i]} for the wonderful ${event} gift !`);
+
  }
      
    return "Thank you, " + names[i] + " for the wonderful " + event+ " gift !";
